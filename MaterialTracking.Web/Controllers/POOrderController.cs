d929b321e70ccdadf0d8fe36d474f925647ad782
Jesse <nhan_j1@denison.edu>
2017-10-19 16:19:38 -0400

    Add files via upload

---
+++ MaterialTracking.Web/Controllers/POOrderController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using MaterialTracking.BL;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.Web.Controllers
+{
+    public class POOrderController : Controller
+    {
+        // GET: POOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region AJAX Actions
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderSelectAll([DataSourceRequest] DataSourceRequest request)
+        {
+            BL.POOrderDetails blPO = new BL.POOrderDetails();
+            List<POOrderDetail> model = blPO.SelectAll();
+            return Json(model.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderUpdate([DataSourceRequest] DataSourceRequest request, POOrderDetail po)
+        {
+            BL.POOrderDetails blVendors = new BL.POOrderDetails();
+            POOrderDetail model = blVendors.Update(po);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderCreate([DataSourceRequest] DataSourceRequest request, POOrderDetail po)
+        {
+            BL.POOrderDetails blVendors = new BL.POOrderDetails();
+            POOrderDetail model = blVendors.Insert(po);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderDestroy([DataSourceRequest] DataSourceRequest request, int po)
+        {
+            BL.POOrderDetails blVendors = new BL.POOrderDetails();
+            POOrderDetail model = blVendors.SoftDeleteByID(po);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion AJAX Actions
+
+    }
+}
---
+++ MaterialTracking.Web/Controllers/POOrderDetailController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using MaterialTracking.BL;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.Web.Controllers
+{
+    public class POOrderDetailController : Controller
+    {
+        // GET: POOrderDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region AJAX Actions
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderDetailSelectAll([DataSourceRequest] DataSourceRequest request)
+        {
+            BL.POOrderDetails blPO = new BL.POOrderDetails();
+            List<POOrderDetail> model = blPO.SelectAll();
+            return Json(model.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderDetailUpdate([DataSourceRequest] DataSourceRequest request, POOrderDetail po)
+        {
+            BL.POOrderDetails blPo = new BL.POOrderDetails();
+            POOrderDetail model = blPo.Update(po);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderDetailCreate([DataSourceRequest] DataSourceRequest request, POOrderDetail po)
+        {
+            BL.POOrderDetails blPo = new BL.POOrderDetails();
+            POOrderDetail model = blPo.Insert(po);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult POOrderDetailDestroy([DataSourceRequest] DataSourceRequest request, int po)
+        {
+            BL.POOrderDetails blPo = new BL.POOrderDetails();
+            POOrderDetail model = blPo.SoftDeleteByID(po);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion AJAX Actions
+
+    }
+}
