09761bd678aef3d6c2fb91ff036fe1c58b967d97
trannon1 <31714387+trannon1@users.noreply.github.com>
2017-10-18 23:13:08 -0400

    Add files via upload

---
+++ Controllers/LocationController - Copy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.Web.Controllers
+{
+    public class LocationController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region AJAX Actions
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult LocationSelectAll([DataSourceRequest] DataSourceRequest request)
+        {
+            BL.Locations blLocations = new BL.Locations();
+            List<Location> model = blLocations.SelectAll();
+            return Json(model.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult LocationUpdate([DataSourceRequest] DataSourceRequest request, Location location)
+        {
+            BL.Locations blLocations = new BL.Locations();
+            Location model = blLocations.Update(location);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult LocationCreate([DataSourceRequest] DataSourceRequest request, Location location)
+        {
+            BL.Locations blLocations = new BL.Locations();
+            Location model = blLocations.Insert(location);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult LocationDestroy([DataSourceRequest] DataSourceRequest request, Location location)
+        {
+            BL.Locations blLocations = new BL.Locations();
+            Location model = blLocations.SoftDelete(location);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion AJAX Actions
+    }
+}
---
+++ ZoneTypes.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.BL
+{
+    public class ZoneTypes : BaseClass
+    {
+        /// <summary>
+        /// Returns all active ZoneTypes
+        /// </summary>
+        /// <returns>Returns the list of all active ZoneTypes</returns>
+        public List<ZoneType> SelectAll()
+        {
+            return repository.Select<ZoneType>(c => c.IsActive);
+        }
+
+        /// <summary>
+        /// Selects a single ZoneType by the ZoneType ID
+        /// </summary>
+        /// <param name="ZoneTypeID">the integer ID of the ZoneType</param>
+        /// <returns>A single ZoneType entity</returns>
+        public ZoneType SelectByID(int ZoneTypeID)
+        {
+            return repository.Select<ZoneType>(c => c.IsActive && c.ZoneTypeID == ZoneTypeID).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Updates the ZoneType record with applied changes
+        /// </summary>
+        /// <param name="ZoneType">The ZoneType record with the ZoneTypeID populated that is to be updated.</param>
+        /// <returns>The ZoneType record back, unaltered.</returns>
+        public ZoneType Update(ZoneType ZoneType)
+        {
+            return repository.Update<ZoneType>(ZoneType);
+        }
+
+        /// <summary>
+        /// Inserts the ZoneType record into the database
+        /// </summary>
+        /// <param name="ZoneType">The new ZoneType record that is to be inserted into the database.</param>
+        /// <returns>The inserted ZoneType record (complete with new ZoneTypeID)</returns>
+        public ZoneType Insert(ZoneType ZoneType)
+        {
+            return repository.Insert<ZoneType>(ZoneType);
+        }
+
+        /// <summary>
+        /// Changes the ZoneType status to not active
+        /// </summary>
+        /// <param name="ZoneType">The ZoneType record that is to be made inactive</param>
+        public ZoneType SoftDelete(ZoneType ZoneType)
+        {
+            ZoneType.IsActive = false;
+            return repository.Update<ZoneType>(ZoneType);
+        }
+
+        /// <summary>
+        /// Changes a ZoneType record of the specified ZoneType ID to be inactive
+        /// </summary>
+        /// <param name="ZoneTypeID">The integer ID of the ZoneType to make inactive </param>
+        public ZoneType SoftDeleteByID(int ZoneTypeID)
+        {
+            ZoneType ZoneType = SelectByID(ZoneTypeID);
+            return SoftDelete(ZoneType);
+        }
+
+    }
+}
