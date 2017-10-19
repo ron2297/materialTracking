829371cc66ff5a6c7a6dd002dc05d035f0250aa2
lannle <le_l1@denison.edu>
2017-10-19 15:38:33 -0400

    Adding Inventory class

---
+++ MaterialTracking.BL/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.BL
+{
+    class InventoryClass:BaseClass
+    {
+  
+        public Inventory SelectByID(int InventoryID)
+        {
+            return repository.Select<Inventory>(c=>c.InventoryID == InventoryID).FirstOrDefault();
+        }
+
+        public Inventory Update(Inventory inventory)
+        {
+            return repository.Update<Inventory>(inventory);
+        }
+
+        public Inventory Insert(Inventory inventory)
+        {
+            return repository.Insert<Inventory>(inventory);
+        }
+
+        public Inventory SoftDelete(Inventory inventory)
+        {
+            return repository.Update<Inventory>(inventory);
+        }
+
+        public Inventory SoftDeleteByID(int InventoryID)
+        {
+            Inventory inventory = SelectByID(InventoryID);
+            return SoftDelete(inventory);
+        }
+
+    }
+}
