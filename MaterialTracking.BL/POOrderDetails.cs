155c737bbf0b95a84d9a27e5c6ff53571f178610
Jesse <nhan_j1@denison.edu>
2017-10-19 16:21:22 -0400

    Add files via upload

---
+++ MaterialTracking.BL/POOrderDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.BL
+{
+    public class POOrderDetails : BaseClass
+    {
+        public List<POOrderDetail> SelectAll()
+        {
+            return repository.Select<POOrderDetail>();
+        }
+
+        public POOrderDetail SelectByID(int POID)
+        {
+            return repository.Select<POOrderDetail>(c => c.POOrderDetailID == POID).FirstOrDefault();
+        }
+
+        public POOrderDetail Update(POOrderDetail pO)
+        {
+            return repository.Update<POOrderDetail>(pO);
+        }
+
+        public POOrderDetail Insert(POOrderDetail pO)
+        {
+            return repository.Insert<POOrderDetail>(pO);
+        }
+
+        public POOrderDetail SoftDeleteByID(int pOID)
+        {
+            POOrderDetail pO = SelectByID(pOID);
+            return repository.Update<POOrderDetail>(pO);
+        }
+
+    }
+}
