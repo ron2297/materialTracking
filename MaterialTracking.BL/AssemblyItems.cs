286f28153eeabfa8556297ae44a9d2ce9a0c00b5
shynobi2p3 <sthila_e1@denison.edu>
2017-10-22 19:37:07 -0400

    With .csproj
    A commit with the .csproj file included in the commit

---
+++ MaterialTracking.BL/AssemblyItems.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.BL
+{
+    public class AssemblyItems : BaseClass
+    {
+        /// <summary>
+        /// Returns all active Products
+        /// </summary>
+        /// <returns>Returns the list of all active Products</returns>
+        public List<AssemblyItem> SelectAll()
+        {
+            return repository.Select<AssemblyItem>(c => c.IsActive);
+        }
+
+        /// <summary>
+        /// Selects a single AssemblyItem by the AssemblyItem ID
+        /// </summary>
+        /// <param name="ProductID">the integer ID of the AssemblyItem</param>
+        /// <returns>A single AssemblyItem entity</returns>
+        public AssemblyItem SelectByID(int AssemblyItemID)
+        {
+            return repository.Select<AssemblyItem>(c => c.IsActive && c.AssemblyItemID == AssemblyItemID).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Updates the AssemblyItem record with applied changes
+        /// </summary>
+        /// <param name="AssemblyItem">The AssemblyItem record with the ProductID populated that is to be updated.</param>
+        /// <returns>The AssemblyItem record back, unaltered.</returns>
+        public AssemblyItem Update(AssemblyItem assemblyItem)
+        {
+            return repository.Update<AssemblyItem>(assemblyItem);
+        }
+
+        /// <summary>
+        /// Inserts the AssemblyItem record into the database
+        /// </summary>
+        /// <param name="AssemblyItem">The new AssemblyItem record that is to be inserted into the database.</param>
+        /// <returns>The inserted AssemblyItem record (complete with new ProductID)</returns>
+        public AssemblyItem Insert(AssemblyItem assemblyItem)
+        {
+            return repository.Insert<AssemblyItem>(assemblyItem);
+        }
+
+        /// <summary>
+        /// Changes the AssemblyItem status to not active
+        /// </summary>
+        /// <param name="AssemblyItem">The AssemblyItem record that is to be made inactive</param>
+        public AssemblyItem SoftDelete(AssemblyItem assemblyItem)
+        {
+            assemblyItem.IsActive = false;
+            return repository.Update<AssemblyItem>(assemblyItem);
+        }
+
+        /// <summary>
+        /// Changes a AssemblyItem record of the specified AssemblyItem ID to be inactive
+        /// </summary>
+        /// <param name="ProductID">The integer ID of the AssemblyItem to make inactive </param>
+        public AssemblyItem SoftDeleteByID(int assemblyItemID)
+        {
+            AssemblyItem AssemblyItem = SelectByID(assemblyItemID);
+            return SoftDelete(AssemblyItem);
+        }
+
+    }
+}
---
+++ MaterialTracking.Web/Controllers/AssemblyItemController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using MaterialTracking.Types;
+
+namespace MaterialTracking.Web.Controllers
+{
+    public class AssemblyItemController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region AJAX Actions
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ProductSelectAll([DataSourceRequest] DataSourceRequest request)
+        {
+            BL.AssemblyItems blProducts = new BL.AssemblyItems();
+            List<AssemblyItem> model = blProducts.SelectAll();
+            return Json(model.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ProductUpdate([DataSourceRequest] DataSourceRequest request, AssemblyItem assemblyItem)
+        {
+            BL.AssemblyItems blProducts = new BL.AssemblyItems();
+            AssemblyItem model = blProducts.Update(assemblyItem);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ProductCreate([DataSourceRequest] DataSourceRequest request, AssemblyItem assemblyItem)
+        {
+            BL.AssemblyItems blProducts = new BL.AssemblyItems();
+            AssemblyItem model = blProducts.Insert(assemblyItem);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ProductDestroy([DataSourceRequest] DataSourceRequest request, AssemblyItem assemblyItem)
+        {
+            BL.AssemblyItems blProducts = new BL.AssemblyItems();
+            AssemblyItem model = blProducts.SoftDelete(assemblyItem);
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion AJAX Actions
+    }
+}
